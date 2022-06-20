6425ced1276a4695ad74e445fce4685f34eea342
Jasin Sowandi <jasinsowandi@gmail.com>
2022-06-20 21:23:23 +0700

    feat:first react website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ photo, name, phone, email }) => {
+  return (
+    <div className="contact">
+      <span>
+        <img className="foto" src={photo} alt="foto"></img>
+      </span>
+      <span>
+        <div className="infoContact">
+          <h4>{name}</h4>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
+      </span>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div className="header">
+      <h1>Call a Friend</h1>
+      <h4>your friendly contact app</h4>
+      <h4 className="bintang">
+        ***************************************************************************
+      </h4>
+    </div>
+  );
+};
 
 export default Header;
