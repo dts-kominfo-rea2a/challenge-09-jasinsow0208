378a60948fde80affdcda54c51bed668e64c2dfa
Jasin Sowandi <jasinsowandi@gmail.com>
2022-06-20 21:33:39 +0700

    fixed:perbaikan app.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,17 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from "react";
 
-const Contact = ({ photo, name, phone, email }) => {
+const Contact = ({ data }) => {
   return (
     <div className="contact">
       <span>
-        <img className="foto" src={photo} alt="foto"></img>
+        <img className="foto" src={data.photo} alt="foto"></img>
       </span>
       <span>
         <div className="infoContact">
-          <h4>{name}</h4>
-          <p>{phone}</p>
-          <p>{email}</p>
+          <h4>{data.name}</h4>
+          <p>{data.phone}</p>
+          <p>{data.email}</p>
         </div>
       </span>
     </div>
